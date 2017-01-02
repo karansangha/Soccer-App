99bddbb4e0d3262acbe7c4b98a8caaf31e1863e5
Karan Sangha <iamkaransangha@gmail.com>
2017-01-02 14:57:29 -0800

    adding a state to players

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -11,49 +11,55 @@ import Player from './Player';
 
 export default class App extends Component {
 
-  getPlayers() {
-    return[
+  constructor(props){
+    super(props);
+
+    //setting up the strategy
+    this.state = { players: [] };
+  }
+
+  componentWillMount() {
+    this.setState({ players: [
       {
-        _id: 1,
-        name: "Karan Sangha",
-        ballManipulation: 2,
-        kickingAbilities: 3,
-        passingAbilities: 1,
-        duelTackling: 1,
-        fieldCoverage: 3,
-        blockingAbilities: 2,
-        gameStrategy: 3,
-        playmakingRisks:2,
-      },
-      {
-        _id: 2,
-        name: "Rajvir Sangha",
-        ballManipulation: 2,
-        kickingAbilities: 3,
-        passingAbilities: 1,
-        duelTackling: 1,
-        fieldCoverage: 3,
-        blockingAbilities: 2,
-        gameStrategy: 3,
-        playmakingRisks:2,
-      },
-      {
-        _id: 3,
-        name: "Jagdish Sangha",
-        ballManipulation: 2,
-        kickingAbilities: 3,
-        passingAbilities: 1,
-        duelTackling: 1,
-        fieldCoverage: 3,
-        blockingAbilities: 2,
-        gameStrategy: 3,
-        playmakingRisks:2,
-      }
-    ];
+      _id: 1,
+      name: "Karan Sangha",
+      ballManipulation: 2,
+      kickingAbilities: 3,
+      passingAbilities: 1,
+      duelTackling: 1,
+      fieldCoverage: 3,
+      blockingAbilities: 2,
+      gameStrategy: 3,
+      playmakingRisks:2,
+    },
+    {
+      _id: 2,
+      name: "Rajvir Sangha",
+      ballManipulation: 2,
+      kickingAbilities: 3,
+      passingAbilities: 1,
+      duelTackling: 1,
+      fieldCoverage: 3,
+      blockingAbilities: 2,
+      gameStrategy: 3,
+      playmakingRisks:2,
+    },
+    {
+      _id: 3,
+      name: "Jagdish Sangha",
+      ballManipulation: 2,
+      kickingAbilities: 3,
+      passingAbilities: 1,
+      duelTackling: 1,
+      fieldCoverage: 3,
+      blockingAbilities: 2,
+      gameStrategy: 3,
+      playmakingRisks:2,
+    }]});
   }
 
   renderPlayers() {
-    return this.getPlayers().map((player) => (
+    return this.state.players.map((player) => (
       <TeamList key={player._id} player={player} />
     ));
   }
