2005b7e1a12d9d744fcb85be61fad0e964a50ea5
Karan Sangha <iamkaransangha@gmail.com>
2017-01-02 01:57:06 -0800

    import card component in Player

--- imports/ui/Player.jsx
+++ imports/ui/Player.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import { Card, CardMedia, CardTitle, CardText, CardActions } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import Avatar from 'material-ui/Avatar';
+import Chip from 'material-ui/Chip';
+import { blue200, blue900} from 'material-ui/styles/colors';
 
 export default class Player extends Component {
   render() {
     return (
-      <h1>Player</h1>
+      <Card>
+        <CardMedia
+          overlay={<CardTitle title="Karan Sangha" subtitle="Offsense: 12 - Defense: 8" />}
+        >
+          <img src="player.jpg" />
+        </CardMedia>
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+        <CardActions>
+        </CardActions>
+    </Card>
     )
   }
 }
--- imports/ui/Team-list.jsx
+++ imports/ui/Team-list.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class TeamList extends Component {
   render() {
     return (
-      <h1>Team list</h1>
+      <h1>Player</h1>
     )
   }
 }
