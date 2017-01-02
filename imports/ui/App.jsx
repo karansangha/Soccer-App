55cd6ec778d9db3b421610e3580c06a6b9027064
Karan Sangha <iamkaransangha@gmail.com>
2017-01-02 02:52:14 -0800

    setup a list of players

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,48 @@ import TeamStats from './Team-stats';
 import Player from './Player';
 
 export default class App extends Component {
+
+  getPlayers() {
+    return[
+      {
+        _id: 1,
+        name: "Karan Sangha",
+        ballManipulation: 2,
+        kickingAbilities: 3,
+        passingAbilities: 1,
+        duelTackling: 1,
+        fieldCoverage: 3,
+        blockingAbilities: 2,
+        gameStrategy: 3,
+        playmakingRisks:2,
+      },
+      {
+        _id: 2,
+        name: "Rajvir Sangha",
+        ballManipulation: 2,
+        kickingAbilities: 3,
+        passingAbilities: 1,
+        duelTackling: 1,
+        fieldCoverage: 3,
+        blockingAbilities: 2,
+        gameStrategy: 3,
+        playmakingRisks:2,
+      },
+      {
+        _id: 3,
+        name: "Jagdish Sangha",
+        ballManipulation: 2,
+        kickingAbilities: 3,
+        passingAbilities: 1,
+        duelTackling: 1,
+        fieldCoverage: 3,
+        blockingAbilities: 2,
+        gameStrategy: 3,
+        playmakingRisks:2,
+      },
+    ]
+  }
+
   render() {
     return (
       <MuiThemeProvider>
