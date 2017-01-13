1bde8717901dad461e31b6ae342f5d5777186531
Karan Sangha <iamkaransangha@gmail.com>
2017-01-13 12:57:40 -0800

    add Player schema

---
+++ imports/api/players.js
@@ -0,0 +1,19 @@
+import { Mongo } from 'meteor/mongo';
+
+export const Players = new Mongo.Collection('players');
+
+const PlayerSchema = new SimpleSchema({
+  name: { type: String },
+  team: { type: String },
+  ballManipulation: { type: Number, defaultValue: 0 },
+  kickingAbilities: { type: Number, defaultValue: 0 },
+  passingAbilities: { type: Number, defaultValue: 0 },
+  duelTackling: { type: Number, defaultValue: 0 },
+  fieldCoverage: { type: Number, defaultValue: 0 },
+  blockingAbilities: { type: Number, defaultValue: 0 },
+  gameStrategy: { type: Number, defaultValue: 0 },
+  playmakingRisks: { type: Number, defaultValue: 0 },
+  notes: { type: String, optional: true},
+})
+
+Players.attatchSchema(PlayerSchema);
